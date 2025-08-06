2e8ed28b400137feb429d1239ac4e7817d972bd4
kartik <kartik200421@gmail.com>
2025-08-06 13:45:28 +0530

    docker files added

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,22 @@
 import { prisma } from "db";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
   try {
-    const data = await prisma.user.findMany();
-    return <div>{JSON.stringify(data)}</div>;
+    const users = await prisma.user.findMany();
+
+    return (
+      <div>
+        {users.map((user, i) => (
+          <div key={i}>
+            {user.name} || {user.age} || {user.id}
+          </div>
+        ))}
+      </div>
+    );
   } catch (error) {
-    console.error('Database connection failed:', error);
+    console.error("Database connection failed:", error);
     return <div>Database connection failed</div>;
   }
 }
